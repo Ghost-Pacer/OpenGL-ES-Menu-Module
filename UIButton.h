cae298b56208af2c095e89c7162f64856c99fc8f
sidHathi <sidhathi20@gmail.com>
2021-08-03 21:24:45 -0700

    Added some documentation

--- UIButton.h
+++ UIButton.h
@@ -1,12 +1,12 @@
 /******************************************************************************
 
- @File          UIBadges.h
+ @File          UIButton.h
 
- @Title         UIBadges Header
+ @Title         UIButton Header
 
  @Author        Siddharth Hathi
 
- @Description   Header file for the UIBadge object class. Defines UIBadge
+ @Description   Header file for the UIButton object class. Defines UIButton
 
 ******************************************************************************/
 
@@ -17,6 +17,7 @@
 #ifndef _UIBUTTON_H
 #define _UIBUTTON_H
 
+// Struct used to store initialization info for a UIButton
 struct ButtonData {
 	char* activeBG;
 	char* inactiveBG;
@@ -25,26 +26,57 @@ struct ButtonData {
 	UIFont fontInactive;
 };
 
+// Global constant storing default button appearance
 const ButtonData c_UIBDefaults = {
 	"buttonActive.pvr", "buttonInactive.pvr", 360, 40, 0, 0, 0.18, UIFBold, UIFMedium
 };
 
+/*!****************************************************************************
+ @class UIButton
+ Object class. A UIButton is a peice of text with a background that can dynamically
+ switch between active and inactive states. It stores appearances for both states
+ and it's update function changes the one being displayed.
+******************************************************************************/
 class UIButton : public UIElement
 {
 	protected:
+		// Instance variables
+
+		// Active background image
         UIImage m_activeBG;
+
+		// Inactive background image
 		UIImage m_inactiveBG;
-        // Print2D* m_print2D;
+		
+		// Button text
         char* m_text;
+
+		// Active font
 		UIFont m_fontActive;
+
+		// Inactive font
 		UIFont m_fontInactive;
+
+		// Active text color
         GLuint m_activeColor;
+
+		// Inactive text color
         GLuint m_inactiveColor;
+
+		// Is the button active
 		bool m_active;
+		
+		// Position and sizing
         float m_x, m_y, m_width, m_height, m_insetX, m_insetY, m_textScale;
+		
+		// Is the button hidden
 		bool m_hidden;
+
+		// Enum key used to update the button
 		UIBool m_updateKey;
+
 	public:
+		// Exported functions
         UIButton();
 		UIButton(char* text, float x, float y, GLuint activeColor, GLuint inactiveColor, UIBool updateKey);
         UIButton(char* activeBG, char* inactiveBG, char* text, float x, float y, float width, float height, 
@@ -52,7 +84,6 @@ class UIButton : public UIElement
 		void ToggleActive();
         virtual bool LoadTextures(CPVRTString* const pErrorStr);
         virtual void BuildVertices();
-        // virtual bool Render(GLuint uiMVPMatrixLoc, CPVRTPrint3D* print3D, bool isRotated);
         virtual bool Render(GLuint uiMVPMatrixLoc, UIPrinter* printer);
 		virtual void Update(UIMessage updateMessage);
 		virtual void Hide();
